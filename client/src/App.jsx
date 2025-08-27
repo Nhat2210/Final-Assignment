5f52c855e5662adb891686801b2226cb7ec6ba54
NhatCao <anhnhat22102003@gmail.com>
2025-08-27 17:25:49 +0700

    feat: add Hero Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { useLocation } from "react-router-dom";
+import { useLocation, Route, Routes } from "react-router-dom";
+import Hero from "./components/Hero";
+import Home from "./pages/Home";
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
-      <Navbar />
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
