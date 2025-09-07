72e943841519ab67efb2205fac69db90f7966d50
NhatCao <anhnhat22102003@gmail.com>
2025-09-07 22:26:51 +0700

    feat: add Hotels Booking List

---
+++ client/src/pages/MyBookings.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import Title from "../components/Title";
+import { assets, userBookingsDummyData } from "../assets/assets";
+
+const MyBookings = () => {
+  const [bookings, setBookings] = useState(userBookingsDummyData);
+  console.log(bookings);
+
+  return (
+    <div className="py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32">
+      <Title
+        title="Danh sách đặt phòng"
+        subTitle="Khách hàng hãy tận dụng các chương trình khuyến mãi thời gian có hạn và các gói dịch vụ đặc biệt để nâng tầm trải nghiệm và tạo ra những kỷ niệm đáng nhớ."
+        align="left"
+      />
+
+      <div className="max-w-6xl mt-8 w-full text-gray-800">
+        <div className="hidden md:grid md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 font-medium text-base py-3">
+          <div className="w-1/3">Khách sạn</div>
+          <div className="w-1/3">Thời gian đặt</div>
+          <div className="w-1/3 whitespace-nowrap">Thanh toán</div>
+        </div>
+
+        {bookings.map((booking) => (
+          <div
+            key={booking._id}
+            className="grid grid-cols-1 md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 py-6 first:border-t"
+          >
+            {/* Hotel Details  */}
+            <div className="flex flex-col md:flex-row gap-3">
+              <img
+                src={booking.room.images[0]}
+                alt="image-hotel"
+                className="min-md:w-44 rounded shadow object-cover"
+              />
+              <div className="min-md:w-44 rounded  object-cover">
+                <p className="text-playfair text-2xl">{booking.hotel.name}</p>
+                <span className="font-inter text-sm">
+                  ({booking.room.roomType})
+                </span>
+                <div className="flex items-center gap-1 text-sm text-gray-500">
+                  <img src={assets.locationIcon} alt="location-icon" />
+                  <span>{booking.hotel.address}</span>
+                </div>
+                <div className="flex items-center gap-1 text-sm text-gray-500">
+                  <img src={assets.guestsIcon} alt="guest-icon" />
+                  <span>Guests: {booking.guests}</span>
+                </div>
+                <p className="text-base">Total: ${booking.totalPrice}</p>
+              </div>
+            </div>
+            {/* Date and Timings  */}
+            <div className="flex flex-row md:items-center md:gap-12 mt-3 gap-8">
+              <div>
+                <p>Check-In: </p>
+                <p className="text-gray-500 text-sm">
+                  {new Date(booking.checkInDate).toDateString()}
+                </p>
+              </div>
+              <div>
+                <p>Check-Out: </p>
+                <p className="text-gray-500 text-sm">
+                  {new Date(booking.checkOutDate).toDateString()}
+                </p>
+              </div>
+            </div>
+            {/* Payment Status  */}
+            <div className="flex items-center">
+              <div className="flex flex-col items-center justify-center pt-3">
+                <div className="flex items-center gap-2">
+                  <div
+                    className={`h-3 w-3 rounded-full ${
+                      booking.isPaid ? "bg-green-500" : "bg-red-500"
+                    }`}
+                  ></div>
+                  <p
+                    className={`text-sm ${
+                      booking.isPaid ? "text-green-500" : "text-red-500"
+                    }`}
+                  >
+                    {booking.isPaid ? "Đã thanh toán" : "Chưa thanh toán"}
+                  </p>
+                </div>
+                {!booking.isPaid && (
+                  <button className="px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-50 transition-all cursor-pointer">
+                    Thanh toán ngay
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyBookings;
